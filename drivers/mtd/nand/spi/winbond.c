866828786b907c86ae681b8d31560829f51f9598
MS10 CWWeng <cwweng@nuvoton.com>
2022-03-15 11:29:19 +0800

    Update MTD Winbond SPI NAND driver
    1. Using 2-byte dev id, instead of 1-byte dev id, while scanning SPI NAND ID
    The first byte of dev id are the same among some of the SPI NANDs.
    For instance, both the first byte of "W25N01GV" and "W25N02KVZEIR" are 0xAA.
    Hence, they should be distinguished by 2-byte dev id.
    
    2. Add following Winbond SPI NANd to winbond_spinand_table
    W25N04KWZEIR/W25N04KVZEIR/W25N02KWZEIR/W25N02KVZEIR
    W25N01GVZEIR/W25N01KVZEIG/W25N512GVEIR/W25N512GWEIR
    
    Change-Id: Ib35e180c80f9d1ce775cdacbb330e68e9c1a81be

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -75,7 +75,7 @@ static int w25m02gv_select_target(struct spinand_device *spinand,
 }
 
 static const struct spinand_info winbond_spinand_table[] = {
-	SPINAND_INFO("W25M02GV", 0xAB,
+	SPINAND_INFO("W25M02GV", 0xAB21,
 		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 2),
 		     NAND_ECCREQ(1, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -84,7 +84,7 @@ static const struct spinand_info winbond_spinand_table[] = {
 		     0,
 		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL),
 		     SPINAND_SELECT_TARGET(w25m02gv_select_target)),
-	SPINAND_INFO("W25N02JWZEIF", 0xBF,
+	SPINAND_INFO("W25N02JWZEIF", 0xBF22,
 		     NAND_MEMORG(1, 2048, 64, 64, 2048, 20, 1, 1, 1),
 		     NAND_ECCREQ(1, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -92,7 +92,7 @@ static const struct spinand_info winbond_spinand_table[] = {
 					      &update_cache_variants),
 		     0,
 		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
-	SPINAND_INFO("W25N04KWZEIR", 0xBA,
+	SPINAND_INFO("W25N04KWZEIR", 0xBA23,
 		     NAND_MEMORG(1, 2048, 64, 64, 4096, 20, 1, 1, 1),
 		     NAND_ECCREQ(1, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
@@ -100,8 +100,48 @@ static const struct spinand_info winbond_spinand_table[] = {
 					      &update_cache_variants),
 		     0,
 		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
-	SPINAND_INFO("W25N01GV", 0xAA,
+	SPINAND_INFO("W25N02KWZEIR", 0xBA22,
+		     NAND_MEMORG(1, 2048, 64, 64, 2048, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
+	SPINAND_INFO("W25N04KVZEIR", 0xAA23,
+		     NAND_MEMORG(1, 2048, 64, 64, 4096, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
+	SPINAND_INFO("W25N02KVZEIR", 0xAA22,
+		     NAND_MEMORG(1, 2048, 64, 64, 2048, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
+	SPINAND_INFO("W25N01GV", 0xAA21,
 		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
+	SPINAND_INFO("W25N512GVEIR", 0xBA20,
+		     NAND_MEMORG(1, 2048, 64, 64, 512, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
+	SPINAND_INFO("W25N512GVEIR", 0xAA20,
+		     NAND_MEMORG(1, 2048, 64, 64, 512, 20, 1, 1, 1),
 		     NAND_ECCREQ(1, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
 					      &write_cache_variants,
@@ -119,6 +159,7 @@ static int winbond_spinand_detect(struct spinand_device *spinand)
 {
 	u8 *id = spinand->id.data;
 	int ret;
+	u16 devid;
 
 	/*
 	 * Winbond SPI NAND read ID need a dummy byte,
@@ -127,8 +168,9 @@ static int winbond_spinand_detect(struct spinand_device *spinand)
 	if (id[1] != SPINAND_MFR_WINBOND)
 		return 0;
 
+	devid = (id[2] << 8) + id[3];
 	ret = spinand_match_and_init(spinand, winbond_spinand_table,
-				     ARRAY_SIZE(winbond_spinand_table), id[2]);
+				     ARRAY_SIZE(winbond_spinand_table), devid);
 	if (ret)
 		return ret;
 
